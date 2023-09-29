8c93d6f9bea7a670b2a850849f5ff6b8d9bdb65f
Chuha <chuha91@gmail.com>
2023-09-29 06:01:00 +0300

    task 8 is done

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,24 +1,40 @@
-name = input("Hello, what's your name?")
+name = None
+board_size = None
+number_of_mines = None
 
-if not isinstance(name, str) or len(name) <= 2:
-    print("Your name is too short")
+name_input = input("Hello, what's your name?")
+
+# Check for name conditions
+if isinstance(name_input, str) and len(name_input) > 2:
+    name = name_input
 else:
+    print("Your name is too short")
+
+if name:
     board_size_input = input(f"{name}, please choose board size:")
 
     if board_size_input.isdigit():
-        board_size = int(board_size_input)
+        temp_board_size = int(board_size_input)
 
-        if 0 <= board_size <= 26:
+        # Check for board_size conditions
+        if 0 <= temp_board_size <= 26:
+            board_size = temp_board_size
+
             number_of_mines_input = input(f"{name} for board size {board_size}, choose number of mines to allocate:")
 
             if number_of_mines_input.isdigit():
-                number_of_mines = int(number_of_mines_input)
+                temp_number_of_mines = int(number_of_mines_input)
 
-                if not (0 < number_of_mines < board_size / 2):
-                    print(f"{name}, you entered an illegal number of mines")
+                # Check for number_of_mines conditions
+                if 0 < temp_number_of_mines < board_size / 2:
+                    number_of_mines = temp_number_of_mines
+                else:
+                    print(f"{name} you entered an illegal number of mines")
             else:
-                print(f"{name}, you entered an illegal number of mines")
+                print(f"{name} you entered an illegal number of mines")
         else:
-            print(f"{name}, you entered an illegal board size")
+            print(f"{name} you entered an illegal board size")
     else:
-        print(f"{name}, you entered an illegal board size")
+        print(f"{name} you entered an illegal board size")
+
+print(f"Name: {name}, Board Size: {board_size}, Number of Mines: {number_of_mines}")
