37eaf7c5d264c07575c701fd19b3a1a63686f101
Chuha <chuha91@gmail.com>
2023-09-30 02:53:54 +0300

    The problematic "register_user" call in the end was removed.

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -6,6 +6,7 @@ def is_board_size_valid(board_size):
     return 1 <= board_size <= 26
 
 
+
 def is_number_of_mines_valid(board_size, number_of_mines):
     return 0 < number_of_mines < board_size / 2
 
