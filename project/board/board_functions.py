011099671859880b28850f6213d89f2983911dee
Chuha <chuha91@gmail.com>
2023-09-30 04:19:41 +0300

    assignment_lesson_11

--- project/board/board_functions.py
+++ project/board/board_functions.py
@@ -3,6 +3,7 @@ def is_on_board(x: int, y: int, board: list[list]) -> bool:
         return True
     return False
 
+
 def safe_set_value(x: int, y: int, value: any, board: list[list]) -> bool:
     if is_on_board(x, y, board):
         board[x][y] = value
